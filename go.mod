9a1f2447453ce21827fcd56b662daa3e32235210
oak <ch@beituyun.com>
2022-03-29 20:18:38 +0800

    fix：重构logger，logrus换为zap，新增gorm日志

--- go.mod
+++ go.mod
@@ -5,27 +5,21 @@ go 1.17
 require (
 	github.com/bwmarrin/snowflake v0.3.0
 	github.com/go-redis/redis/v8 v8.11.4
-	github.com/gogf/gf v1.16.6
 	github.com/google/uuid v1.3.0
 	github.com/lestrrat-go/file-rotatelogs v2.4.0+incompatible
 	github.com/shopspring/decimal v1.3.1
 	go.uber.org/zap v1.21.0
 	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9
+	gorm.io/gorm v1.23.3
 )
 
 require (
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
-	github.com/fatih/color v1.12.0 // indirect
-	github.com/fsnotify/fsnotify v1.4.9 // indirect
 	github.com/jonboulle/clockwork v0.2.3 // indirect
 	github.com/lestrrat-go/strftime v1.0.5 // indirect
-	github.com/mattn/go-colorable v0.1.8 // indirect
-	github.com/mattn/go-isatty v0.0.12 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	go.opentelemetry.io/otel v1.0.0-RC2 // indirect
-	go.opentelemetry.io/otel/trace v1.0.0-RC2 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
-	golang.org/x/sys v0.0.0-20210510120138-977fb7262007 // indirect
+	golang.org/x/net v0.0.0-20210520170846-37e1c6afe023 // indirect
 )
---
+++ log/gorm.go
@@ -0,0 +1,84 @@
+package log
+
+import (
+	"context"
+	"errors"
+	"gorm.io/gorm"
+	"time"
+
+	"go.uber.org/zap"
+	gl "gorm.io/gorm/logger"
+)
+
+type GormLogger struct {
+	ZapLogger                 *zap.Logger
+	LogLevel                  gl.LogLevel
+	SlowThreshold             time.Duration
+	SkipCallerLookup          bool
+	IgnoreRecordNotFoundError bool
+}
+
+func NewGormLogger() GormLogger {
+	log := GormLogger{
+		ZapLogger:                 logger,
+		LogLevel:                  gl.Warn,
+		SlowThreshold:             100 * time.Millisecond,
+		SkipCallerLookup:          false,
+		IgnoreRecordNotFoundError: false,
+	}
+	gl.Default = log
+	return log
+}
+func (l GormLogger) LogMode(level gl.LogLevel) gl.Interface {
+	return GormLogger{
+		ZapLogger:                 l.ZapLogger,
+		SlowThreshold:             l.SlowThreshold,
+		LogLevel:                  level,
+		SkipCallerLookup:          l.SkipCallerLookup,
+		IgnoreRecordNotFoundError: l.IgnoreRecordNotFoundError,
+	}
+}
+
+func (l GormLogger) Info(ctx context.Context, str string, args ...interface{}) {
+	if l.LogLevel < gl.Info {
+		return
+	}
+	Debugf(str, args...)
+}
+
+func (l GormLogger) Warn(ctx context.Context, str string, args ...interface{}) {
+	if l.LogLevel < gl.Warn {
+		return
+	}
+	Warnf(str, args...)
+}
+
+func (l GormLogger) Error(ctx context.Context, str string, args ...interface{}) {
+	if l.LogLevel < gl.Error {
+		return
+	}
+	Errorf(str, args...)
+}
+
+func (l GormLogger) Trace(ctx context.Context, begin time.Time, fc func() (string, int64), err error) {
+	if l.LogLevel <= 0 {
+		return
+	}
+	elapsed := time.Since(begin)
+	switch {
+	case err != nil && l.LogLevel >= gl.Error && (!l.IgnoreRecordNotFoundError || !errors.Is(err, gorm.ErrRecordNotFound)):
+		sql, rows := fc()
+		Error("trace", zap.Error(err), zap.Duration("elapsed", elapsed), zap.Int64("rows", rows), zap.String("sql", sql))
+	case l.SlowThreshold != 0 && elapsed > l.SlowThreshold && l.LogLevel >= gl.Warn:
+		sql, rows := fc()
+		Warn("trace", zap.Duration("elapsed", elapsed), zap.Int64("rows", rows), zap.String("sql", sql))
+	case l.LogLevel >= gl.Info:
+		sql, rows := fc()
+		Debug("trace", zap.Duration("elapsed", elapsed), zap.Int64("rows", rows), zap.String("sql", sql))
+	}
+}
+
+//
+//func (l GormLogger) logger() *zap.Logger {
+//	return l.ZapLogger.WithOptions(zap.AddCallerSkip(2))
+//}
